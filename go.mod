9c0a64eaba7b0b740b06be8d567f978343f219b3
jjeejj <jwj@wenjunjiang.com>
2022-11-25 23:19:46 +0800

    feat:  func

--- go.mod
+++ go.mod
@@ -3,6 +3,13 @@ module github.com/jjeejj/go-crawler
 go 1.19
 
 require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/antchfx/htmlquery v1.2.5 // indirect
+	github.com/antchfx/xpath v1.2.1 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/sirupsen/logrus v1.9.0 // indirect
-	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	golang.org/x/net v0.2.0 // indirect
+	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,30 +1,66 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"io/ioutil"
 	"net/http"
 
+	"github.com/antchfx/htmlquery"
 	log "github.com/sirupsen/logrus"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
 )
 
 func main() {
 	url := "https://www.thepaper.cn"
+	body, err := Fetch(url)
+	if err != nil {
+		log.Errorf("read content failed err: %v", err)
+		return
+	}
+	// log.Infof("body: %v", string(body))
+	// numLinks := strings.Count(string(body), "<a")
+	// log.Infof("home page has %d links", numLinks)
+	doc, err := htmlquery.Parse(bytes.NewReader(body))
+	// log.Infof("%v:", doc.Data)
+	if err != nil {
+		log.Errorf("htmlquery.Parse failed err: %v", err)
+		return
+	}
+	nodes := htmlquery.Find(doc, `//li[@class="index_wechartcontent__yM1tu"]/span`)
+	for _, node := range nodes {
+		log.Info(node.FirstChild.Data)
+	}
+
+}
+
+func Fetch(url string) ([]byte, error) {
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Errorf("fetch url error: %v", err)
-		return
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
 		log.Errorf("Error status code: %v: ", resp.StatusCode)
-		return
+		return nil, err
 	}
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DetermineEncoding(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	return ioutil.ReadAll(utf8Reader)
+}
 
-	body, err := ioutil.ReadAll(resp.Body)
+func DetermineEncoding(r *bufio.Reader) encoding.Encoding {
+	bytes, err := r.Peek(1024)
 	if err != nil {
-		log.Errorf("read content failed: %v", err)
-		return
+		log.Errorf("r.Peek error: %v", err)
+		return unicode.UTF8
 	}
-	log.Infof("body: %v", string(body))
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
 }
